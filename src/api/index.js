d5f0ece3bc58a667709603ba7f1ba81c655ecb99
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-02-16 23:26:28 -0500

    forgot to change baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //   baseURL: "https://cygapi.herokuapp.com/api"
-  baseURL: "http://localhost:3000/api"
+    baseURL: "https://cygapi.herokuapp.com/api"
+  // baseURL: "http://localhost:3000/api"
 });
 
 export const getAllComments = () => api.get("/comments");
